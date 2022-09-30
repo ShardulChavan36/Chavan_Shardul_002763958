0bac5fcdf6d6f9672477324af707a81b7e6732f7
ShardulChavan36 <chavan.sha@northeastern.edu>
2022-09-30 03:47:52 -0400

    Interface Revised

--- src/model/EmpData.java
+++ src/model/EmpData.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
+import java.util.ArrayList;
 
 /**
  *
  * @author chava
  */
 public class EmpData {
+    private ArrayList<EmpDetails> saved_data;
+    public EmpData(){
+         this.saved_data=new ArrayList<EmpDetails>();      
+         
+    }
+
+    public ArrayList<EmpDetails> getSaved_data() {
+        return saved_data;
+    }
+
+    public void setSaved_data(ArrayList<EmpDetails> saved_data) {
+        this.saved_data = saved_data;
+    }
+    public EmpDetails saveEmpDetails(){
+        EmpDetails emp=new EmpDetails();
+        saved_data.add(emp);
+        return emp;
+    }
+    
     
 }
--- src/model/EmpDetails.java
+++ src/model/EmpDetails.java
@@ -3,11 +3,109 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-
+import java.util.Date;
+import javax.swing.*;
+import java.time.LocalDate;
 /**
  *
  * @author chava
  */
 public class EmpDetails {
+    private String name;
+    private String gender;
+    private String team_info;
+    private String Pos_title;
+    private String email_id;
+    
+    private int age;
+    private int empid;
+    private long cell_no;
+    private LocalDate start_date;
+    private ImageIcon icon;
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getTeam_info() {
+        return team_info;
+    }
+
+    public void setTeam_info(String team_info) {
+        this.team_info = team_info;
+    }
+
+    public String getPos_title() {
+        return Pos_title;
+    }
+
+    public void setPos_title(String Pos_title) {
+        this.Pos_title = Pos_title;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public long getCell_no() {
+        return cell_no;
+    }
+
+    public void setCell_no(long cell_no) {
+        this.cell_no = cell_no;
+    }
+
+
+    public ImageIcon getIcon() {
+        return icon;
+    }
+
+    public void setIcon(ImageIcon icon) {
+        this.icon = icon;
+    }
+
+    public EmpDetails saveEmpDetails() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
